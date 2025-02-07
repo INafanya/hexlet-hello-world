ed58a29ebc0bb650c81a4e20b264eb76956e4682
Andrey Solovyov <solovyov.net@gmail.com>
2025-02-07 20:44:06 +0300

    modify example.py

--- hexlet-hello-world/example.py
+++ hexlet-hello-world/example.py
@@ -1,1 +1,9 @@
-print("Hello from python project!")
+from more_itertools import flatten, substrings
+
+coll = [(0, 1), (2, 3)]
+# делаем коллекцию плоской
+print(list(flatten(coll))) # => [0, 1, 2, 3]
+
+# получаем все подстроки строки
+print([''.join(s) for s in substrings('more')])
+# => ['m', 'o', 'r', 'e', 'mo', 'or', 're', 'mor', 'ore', 'more']
