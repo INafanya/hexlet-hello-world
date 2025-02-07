62b902c6c70220949c5d82ff309be1545a49deb6
Andrey Solovyov <solovyov.net@gmail.com>
2025-02-07 22:43:25 +0300

    add ruff

--- hexlet-hello-world/example.py
+++ hexlet-hello-world/example.py
@@ -2,8 +2,8 @@ from more_itertools import flatten, substrings
 
 coll = [(0, 1), (2, 3)]
 # делаем коллекцию плоской
-print(list(flatten(coll))) # => [0, 1, 2, 3]
+print(list(flatten(coll)))  # => [0, 1, 2, 3]
 
 # получаем все подстроки строки
-print([''.join(s) for s in substrings('more')])
+print(["".join(s) for s in substrings("more")])
 # => ['m', 'o', 'r', 'e', 'mo', 'or', 're', 'mor', 'ore', 'more']
